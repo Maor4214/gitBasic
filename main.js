f875061b5bc8b7701f3d0684408c32277782e7e8
Maor <Maoryad@gmail.com>
2024-10-27 20:19:05 +0200

    Limit Size in Function

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ var gBallSize = 100;
 
 function onBallClick(elBall) {
   var newBallSize = gBallSize + 50;
-  elBall.style.height = newBallSize + "px";
-  elBall.style.width = newBallSize + "px";
-  gBallSize = newBallSize;
-  elBall.innerText = gBallSize;
+  if (gBallSize === 400) {
+    elBall.style.height = "100px";
+    elBall.style.width = "100px";
+    gBallSize = 100;
+    elBall.innerText = gBallSize;
+  } else {
+    elBall.style.height = newBallSize + "px";
+    elBall.style.width = newBallSize + "px";
+    gBallSize = newBallSize;
+    elBall.innerText = gBallSize;
+  }
 }
